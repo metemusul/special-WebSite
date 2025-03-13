419e1f482b183d92d45124dabf9124f8c7f31c09
metemusul <115950606+metemusul@users.noreply.github.com>
2025-03-13 19:20:03 +0300

    Add files via upload

---
+++ artibiz/hakkımızda.blade.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Topic Listing Contact Page</title>
+
+        <!-- CSS FILES -->        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Open+Sans&display=swap" rel="stylesheet">
+                        
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="css/bootstrap-icons.css" rel="stylesheet">
+
+        <link href="css/templatemo-topic-listing.css" rel="stylesheet">
+<!--
+
+TemplateMo 590 topic listing
+
+https://templatemo.com/tm-590-topic-listing
+
+-->
+    </head>
+    
+    <body class="topics-listing-page" id="top">
+
+        <main>
+
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
+                    <a class="navbar-brand" href="index.html">
+                    <img style="height: 70px; " src="images/logo.png" alt="">
+                        <span>Artı Biz</span>
+                    </a>
+
+                    
+    
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+    
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-lg-5 me-lg-auto">
+                            <li class="nav-item">
+                                <a class="nav-link " href="index.html">ana sayfa</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link active" href="about.html">hakkımızda</a>
+                            </li>
+    
+                        
+
+                            <li class="nav-item">
+                                <a class="nav-link " href="gallery.html">Galerİ</a>
+                            </li>
+    
+                            <li class="nav-item">
+                                <a class="nav-link " href="contact.html">İLETİŞİM</a>
+                            </li>
+
+                         
+                        </ul>
+
+                     
+                 
+                    <div class="d-none d-lg-block">
+                      <a href="#top" class="navbar-icon bi-house smoothscroll"></a>
+                  </div>
+                  
+                      
+                      
+                    </div>
+                </div>
+            </nav>
+
+
+            <header class="site-header d-flex flex-column justify-content-center align-items-center">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-lg-5 col-12">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Ana Sayfa</a></li>
+
+                                    <li class="breadcrumb-item active" aria-current="page">Hakkımızda</li>
+                                </ol>
+                            </nav>
+
+                            <h2 class="text-white">HAKKIMIZDA</h2>
+                        </div>
+
+                    </div>
+                </div>
+            </header>
+
+
+         
+        </main>
+
+
+<!-- About Start -->
+<div class="container-fluid py-5 about bg-light">
+    <div class="container py-5">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                <div class="video border">
+                    <button type="button" class="btn btn-play" data-bs-toggle="modal" data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
+                        <span></span>
+                    </button>
+                </div>
+            </div>
+            <div class="col-lg-7 wow fadeIn" data-wow-delay="0.3s">
+                <h4 class="text-primary mb-4 border-bottom border-primary border-2 d-inline-block p-2 title-border-radius">Hakkımızda</h4>
+                <h1 class="text-dark mb-4 display-5">Çocuklarınız İçin Aydınlık Bir Gelecek İçin Akıllı Öğrenme Yöntemleri</h1>
+                <p class="text-dark mb-4">Okulumuzda, çocuklarınızın potansiyellerini en üst düzeye çıkarabilecekleri yenilikçi ve öğrenci odaklı eğitim programları sunuyoruz. Eğitim sistemimiz, bireysel ihtiyaçlara odaklanarak, her çocuğun benzersiz yeteneklerini keşfetmelerine yardımcı olacak şekilde tasarlanmıştır. Ayrıca, eğitimin yanı sıra çeşitli kurslar ve etkinliklerle öğrencilerimizin kişisel gelişimini desteklemekteyiz.</p>
+                <div class="row mb-4">
+                    <div class="col-lg-6">
+                        <h6 class="mb-3"><i class="fas fa-check-circle me-2"></i>Sanat ve Spor Kursları</h6>
+                        <h6 class="mb-3"><i class="fas fa-check-circle me-2 text-primary"></i>Açık Hava Etkinlikleri</h6>
+                        <h6 class="mb-3"><i class="fas fa-check-circle me-2 text-secondary"></i>Sağlıklı Beslenme Programları</h6>
+                    </div>
+                    <div class="col-lg-6">
+                        <h6 class="mb-3"><i class="fas fa-check-circle me-2"></i>Güvenli ve Kontrol Edilen Ortam</h6>
+                        <h6 class="mb-3"><i class="fas fa-check-circle me-2 text-primary"></i>Dostça ve Destekleyici Eğitim Ortamı</h6>
+                        <h6><i class="fas fa-check-circle me-2 text-secondary"></i>Nitelikli ve Deneyimli Öğretmenler</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Video -->
+<div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content rounded-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tanıtım Videosu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- 16:9 aspect ratio -->
+                <div class="ratio ratio-16x9">
+                    <iframe class="embed-responsive-item" src="" id="video" allowfullscreen allowscriptaccess="always" allow="autoplay"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About End -->
+
+
+<div class="widget-boxes">
+    <div class="box">
+        <a href="#">
+            <div class="box-info about">
+                <h2>Okulumuz </h2>
+                <p>Öğrencilerimize yaratıcı düşünme becerilerini geliştiren bir eğitim sunuyoruz. Öğretim kadromuz, her öğrencinin potansiyelini keşfetmesi için çalışmaktadır.</p>
+            </div>
+        </a>
+    </div>
+    <div class="box">
+        <a href="#">
+            <div class="box-info experience">
+                <h2>Deneyimimiz</h2>
+                <p>Yıllardır çeşitli eğitim programları ile çocukların gelişimlerine katkı sağlıyoruz. Eğitmenlerimiz, bireysel yetenekleri keşfetmelerine yardımcı olmaktadır.</p>
+            </div>
+        </a>
+    </div>
+    <div class="box">
+        <a href="#">
+            <div class="box-info fun">
+                <h2>Eğlence!</h2>
+                <p>Okulumuzda çocukların sosyal becerilerini geliştiren eğlenceli etkinlikler sunulmaktadır. Spor, sanat ve çeşitli kurslarla dolu bir ortamda eğitim verilmektedir.</p>
+            </div>
+        </a>
+    </div>
+</div>
+
+
+        
+
+<footer class="site-footer section-padding">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-3 col-12 mb-4 pb-2">
+                <a class="navbar-brand mb-2" href="index.html">
+                  <img style="height: 70px; " src="images/logo.png" alt="">
+                  <span>Artı Biz</span>
+                </a>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-6">
+                <h6 class="site-footer-title mb-3">Kaynaklar</h6>
+
+                <ul class="site-footer-links">
+                    <li class="site-footer-link-item">
+                        <a href="index.html" class="site-footer-link">Ana Sayfa</a>
+                    </li>
+
+                    <li class="site-footer-link-item">
+                        <a href="about.html" class="site-footer-link">Hakkımızda</a>
+                    </li>
+
+                    <li class="site-footer-link-item">
+                        <a href="gallery.html" class="site-footer-link">Galeri</a>
+                    </li>
+
+                    <li class="site-footer-link-item">
+                        <a href="contact.html" class="site-footer-link">İletişim</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-6 mb-4 mb-lg-0">
+                <h6 class="site-footer-title mb-3">İletişim</h6>
+
+                <p class="text-white d-flex mb-1">
+                    <a href="tel:  +90 537 289 51 00" class="site-footer-link">
+                      +90 537 289 51 00
+                    </a>
+                </p>
+
+                <p class="text-white d-flex">
+                    <a href="mailto:artibiz2323@gmail.com
+                    " class="site-footer-link">
+                      artibiz2323@gmail.com
+                    </a>
+                </p>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-12 mt-4 mt-lg-0 ms-auto">
+           
+
+               
+                Designed By  <a rel="nofollow" href="https://www.linkedin.com/in/metehan-mu%C5%9Ful-7b8611251/" target="_blank">Metehan Muşul</a>  <a href="https://www.linkedin.com/in/mustafa-cihan-incir-b53393247/">Mustafa Cihan İncir</a></p>
+                
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+        <!-- JAVASCRIPT FILES -->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/custom.js"></script>
+
+    </body>
+</html>
